acaa451f274620e7bc806117599c92a3546f463b
Michael <86883508+Geyerlechner@users.noreply.github.com>
2022-12-01 15:31:56 +0100

    add save

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,12 +3,19 @@ namespace App\Http\Controllers;
 
 use App\Contact;
 use App\User;
+use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function show()
     {
         return response()->json(Contact::all());
-
     }
+
+    public function save(Request $request)
+    {
+        $contacts = Contact::create($request->all());
+        return response()->json($contacts, 201);
+    }
+
 }

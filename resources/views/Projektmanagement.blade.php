95f8f52c3068269672b42c9f47331c93eac387e7
Paul.Sickinger <Paul.Sickinger@students.bs-linz2.ac.at>
2022-12-07 13:27:34 +0100

    HTML seite Projektmanagement.blade.php und bilder vielleicht kann man die nutzn juhu

---
+++ resources/views/Projektmanagement.blade.php
@@ -0,0 +1,143 @@
+<html>
+<head><title> Anfahrtsseite</title>
+</head>
+<body>
+<header>
+
+</header>
+<main>
+    <section>
+        <h1>Mechanische Konstruktion in Autodesk Inventor und Solidworks</h1>
+        <div>
+            <i>
+
+            </i>
+            <p>
+                Entwurf und Entwicklung von Konzepten
+            </p>
+        </div>
+        <p> Konstruktion von </p>
+        <div>
+            <i>
+
+            </i>
+            <p> Sondermaschienen </p>
+        </div>
+
+        <div>
+            <i>
+
+            </i>
+
+            <p> Vorrichtungen </p>
+        </div>
+
+        <div>
+            <i>
+
+            </i>
+            <p> Blech- und Schweißkonstruktionen </p>
+        </div>
+    </section>
+    <section>
+        <h1> Electroplanung in EPlan Electric P8 incl. Propanel und WSCAD suite </h1>
+        <div>
+            <i>
+
+            </i>
+            <p> Hardwareplanung; </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Stromlaufpläne; </p>
+        </div>
+
+        <div>
+            <i>
+
+            </i>
+            <p> Schaltschrankaufbau in 3D </p>
+        </div>
+        <img src="">
+    </section>
+    <section>
+        <h1> Softwareentwicklung </h1>
+        <div>
+            <i>
+
+            </i>
+            <p> SPS-Programmierung(Siemens, Vipa...) </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Anlagenvisualisierung(Movicon, WinCC </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Roboterprogrammierung (Adept, Kuka, ABB...) </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Betriebs- & Maschienendatenerfassung(Microsoft Visual Studio) </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Messtechnik (elektrische sowie mechanische Größen) </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Fernwartung unserer Anlagen weltweit </p>
+            <img src="">
+        </div>
+    </section>
+    <section>
+        <h1> Vision-Systeme(Keyence,...) </h1>
+        <div>
+            <i>
+
+            </i>
+            <p> Bauteilinspektion </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Qualitätskontrolle </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Messungen </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> Löt-/ Schweißstellenkontrolle </p>
+        </div>
+        <div>
+            <i>
+
+            </i>
+            <p> 3D-Inspektion </p>
+        </div>
+    </section>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>

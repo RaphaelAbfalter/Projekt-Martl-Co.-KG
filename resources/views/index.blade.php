47f4cf82f61dd08f625a0ddf610a1849fe276ce2
leonie zehentmayer <leonie.zehentmayer@students.bs-linz2.ac.at>
2022-12-07 11:44:45 +0100

    startseite entwurf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,49 @@
 <!doctype html>
-<html lang="en">
-  <head>
+<html lang="de">
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Martl-Co-KG</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Martl-Co-KG</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
+<body>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                  <a class="nav-link" href="#">Anmelden</a>
-              </li>
+@include('header')
 
-          </ul>
-      </div>
-  </nav>
+<main>
+    <section id="upperPage">
+        <span>
 
-  <div class="container">
-      <div class="row">
-          <div class="col"><br>
-              @include('kontakt')
-          </div>
+        </span>
 
-      </div>
-  </div>
+        <span>
+      <p>Text</p>
+        </span>
 
+        <span>
 
-  @include('footer')
+        </span>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+        <img id="" src="test" alt=""/>
+        <img id="" src="test2" alt=""/>
+        <img id="" src="test3" alt=""/>
+    </section>
+
+    <section id="lowerPage">
+        <div>
+            <img id="" src="test4" alt=""/>
+            <p>Adresse</p>
+        </div>
+    </section>
+
+</main>
+
+@include('footer')
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
 </html>

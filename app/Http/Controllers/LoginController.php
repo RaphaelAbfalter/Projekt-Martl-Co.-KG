19cf9bb3e93a015d5caf14b74b8971169bdc4871
Michael <86883508+Geyerlechner@users.noreply.github.com>
2022-12-07 10:26:02 +0100

    add login function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Contact;
+use App\Login;
 use App\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function show()
+    {
+        return response()->json(Login::all());
+    }
+
+    public function login(Request $request)
     {
         
     }

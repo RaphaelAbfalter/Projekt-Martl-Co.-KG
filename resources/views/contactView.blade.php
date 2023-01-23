43834a222d821097d75facd10bc4243b4d023b87
Noel <96701996+noesti@users.noreply.github.com>
2023-01-23 20:21:14 +0100

    contactView

--- resources/views/contactView.blade.php
+++ resources/views/contactView.blade.php
@@ -1,24 +1,22 @@
 @extends('dashboard')
 @section('content')
-
-    <div>
-        <table class="table table-light" width="100px">
-            <thead>
+<main class="content-container">
+    <table class="contacts-table">
+        <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">E-Mail</th>
-                <th scope="col">Nachricht</th>
-                <th scope="col">Datum</th>
-                <th scope="col"></th>
-                <th scope="col"></th>
+                <th>#</th>
+                <th>Name</th>
+                <th>E-Mail</th>
+                <th>Nachricht</th>
+                <th>Datum</th>
+                <th></th>
+                <th></th>
             </tr>
-            </thead>
-            <tbody>
-
+        </thead>
+        <tbody>
             @foreach ($data as $user)
             <tr>
-                <th scope="row">{{ $user['id'] }}</th>
+                <th>{{ $user['id'] }}</th>
                 <td>{{ $user['name'] }}</td>
                 <td>{{ $user['email'] }}</td>
                 <td>{{ substr($user['message'], 0, 20) }}</td>
@@ -27,7 +25,7 @@
                 <td><a href="/delete/{{ $user['id'] }}">Löschen</a></td>
             </tr>
             @endforeach
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
+</main>
 @endsection

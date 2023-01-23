60a5db771be7d09d2f191823b81b4258ee161dfb
Noel <96701996+noesti@users.noreply.github.com>
2023-01-23 23:08:42 +0100

    cleanup

--- resources/views/projektmanagement.blade.php
+++ resources/views/projektmanagement.blade.php
@@ -17,23 +17,23 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z"/>
             </svg>
-            <p>Sondermaschienen </p>
+            <p>Sondermaschienen</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Vorrichtungen </p>
+            <p>Vorrichtungen</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Blech- und Schweißkonstruktionen </p>
+            <p>Blech- und Schweißkonstruktionen</p>
         </div>
     </section>
     <section class="elec-plan">
-        <h1>Electroplanung in EPlan Electric P8 incl. Propanel und WSCAD suite </h1>
+        <h1>Electroplanung in EPlan Electric P8 incl. Propanel und WSCAD suite</h1>
         <div class="info-card">
         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
             <path d="M4.5 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zM3 4.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm2 7a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-2.5.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
@@ -52,85 +52,85 @@
             <i>
 
             </i>
-            <p>Schaltschrankaufbau in 3D </p>
+            <p>Schaltschrankaufbau in 3D</p>
         </div>
         <div class="img-contain">
             <img src="./assets/Elektroplanung.png" alt="Elektroplanung">
         </div>
     </section>
     <section class="soft-entw">
-        <h1>Softwareentwicklung </h1>
+        <h1>Softwareentwicklung</h1>
         <div class="info-card">
             <i>
 
             </i>
-            <p>SPS-Programmierung (Siemens, Vipa...) </p>
+            <p>SPS-Programmierung (Siemens, Vipa...)</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Anlagenvisualisierung (Movicon, WinCC </p>
+            <p>Anlagenvisualisierung (Movicon, WinCC)</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Roboterprogrammierung (Adept, Kuka, ABB...) </p>
+            <p>Roboterprogrammierung (Adept, Kuka, ABB...)</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Betriebs- & Maschienendatenerfassung (Microsoft Visual Studio) </p>
+            <p>Betriebs- & Maschienendatenerfassung (Microsoft Visual Studio)</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Messtechnik (elektrische sowie mechanische Größen) </p>
+            <p>Messtechnik (elektrische sowie mechanische Größen)</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Fernwartung unserer Anlagen weltweit </p>
+            <p>Fernwartung unserer Anlagen weltweit</p>
         </div>
         <div class="img-contain">
             <img src="./assets/Softwareentwicklung.png" alt="Softwareentwicklung">
         </div>
     </section>
     <section class="visi-syst">
-        <h1>Vision-Systeme (Keyence,...) </h1>
+        <h1>Vision-Systeme (Keyence,...)</h1>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Bauteilinspektion </p>
+            <p>Bauteilinspektion</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Qualitätskontrolle </p>
+            <p>Qualitätskontrolle</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Messungen </p>
+            <p>Messungen</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>Löt-/Schweißstellenkontrolle </p>
+            <p>Löt-/Schweißstellenkontrolle</p>
         </div>
         <div class="info-card">
             <i>
 
             </i>
-            <p>3D-Inspektion </p>
+            <p>3D-Inspektion</p>
         </div>
     </section>
 </main>

05480121a69739d11ff1f12ddb52388e1b709275
Noel <96701996+noesti@users.noreply.github.com>
2023-01-24 21:42:27 +0100

    download

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -6,36 +6,30 @@
         @if(isset($no_files) && $no_files)
         <p>There are no files available for download.</p>
         @else
-        <ul>
             @foreach($files as $file)
-            <li>
-                <a href="{{ route('download.file', $file->id) }}">{{ $file->fileName }}</a>
-            </li>
+            <a href="{{ route('download.file', $file->id) }}">{{ $file->fileName }}</a>
             @endforeach
-        </ul>
         @endif
-        <br>
+    </section>
+    <section class="download-section">
         @if($isAdmin)
             <form method="post" action="{{ route('upload') }}" enctype="multipart/form-data">
                 @csrf
                 <label for="file">Select file to upload:</label>
                 <input type="file" name="file" id="file" required>
-                <br>
                 <label for="access_level">Select access level:</label>
                 <select id="access_level" name="access_level" required>
                     <option value="all">All Users</option>
                     <option value="specific">Specific Users</option>
                 </select>
-                <br>
-                <div id="specific_users" style="display: none">
+                <span id="specific_users" style="display: none">
                     <label for="user">Select specific users:</label>
                     <select id="user" name="user">
                         @foreach($users as $user)
                             <option value="{{ $user->id }}">{{ $user->name }}</option>
                         @endforeach
                     </select>
-                </div>
-                <br>
+                </span>
                 <input type="submit" value="Hochladen" class="style-button form-button">
             </form>
         @endif
@@ -44,7 +38,7 @@
 <script>
     document.getElementById("access_level").addEventListener("change", function(){
         if(this.value === "specific"){
-            document.getElementById("specific_users").style.display = "block";
+            document.getElementById("specific_users").style.display = "flex";
         } else {
             document.getElementById("specific_users").style.display = "none";
         }

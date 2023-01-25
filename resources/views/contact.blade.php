676ae126131776415cf301ab0539ea5d592fb515
NoelStiftner <119508137+NoelStiftner@users.noreply.github.com>
2023-01-25 09:20:22 +0100

    error-msg

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,9 @@
 <form action="{{ route('contact.create') }}" method="post" class="contact-create">
     @csrf
         <label for="contact-name">Vorname und Nachname:</label>
+        @if ($errors->has('name'))
+            <span class="error-msg">{{ $errors->first('name') }}</span>
+        @endif
         <input type="text" class="input-design" name="name" id="contact-name" placeholder="Name">
 
     @if ($errors->has('name') OR $errors->has('email') OR $errors->has('message'))
@@ -11,19 +14,16 @@
         @endphp
     @endif
 
-    @if ($errors->has('name'))
-        <span class="error-msg">{{ $errors->first('name') }}</span>
-    @endif
     <label for="mail">E-Mail:</label>
+        @if ($errors->has('email'))
+            <span class="error-msg">{{ $errors->first('email') }}</span>
+        @endif
     <input type="email" class="input-design" name="email" id="mail" placeholder="name@example.com">
-    @if ($errors->has('email'))
-        <span class="error-msg">{{ $errors->first('email') }}</span>
-    @endif
     <label for="message">Nachricht:</label>
+        @if ($errors->has('message'))
+            <span class="error-msg">{{ $errors->first('message') }}</span>
+        @endif
     <textarea class="input-design textarea-design" name="message" id="message" placeholder="Nachricht"></textarea>
-    @if ($errors->has('message'))
-        <span class="error-msg">{{ $errors->first('message') }}</span>
-    @endif
     <input type="submit" value="Absenden" class="style-button form-button"/>
 </form>
 </div>
